5986181328c0bc8de3e49c53e60610379949d9c8
amigin <amigin@gmail.com>
2022-09-03 00:40:19 +0300

    Added field checks

--- src/generator/generate.rs
+++ src/generator/generate.rs
@@ -1,9 +1,12 @@
 use proc_macro::TokenStream;
 
+use crate::reflection::StructProperty;
+
 pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
 
     let fields = crate::reflection::StructProperty::read(ast);
+    check_mandatory_fields(&fields);
 
     let struct_name = name.to_string();
 
@@ -26,4 +29,48 @@ pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
     result.push_str("}\n");
 
     result.parse().unwrap()
+}
+
+const PARTITION_KEY_FIELD: &str = "partition_key";
+const ROW_KEY_FIELD: &str = "row_key";
+const TIMESTAMP_FIELD: &str = "timestamp";
+
+fn check_mandatory_fields(fields: &[StructProperty]) {
+    let mut partition_key = None;
+    let mut row_key = None;
+    let mut timestamp = None;
+
+    for field in fields {
+        if field.name == PARTITION_KEY_FIELD {
+            partition_key = Some(field);
+        } else if field.name == ROW_KEY_FIELD {
+            row_key = Some(field);
+        } else if field.name == TIMESTAMP_FIELD {
+            timestamp = Some(field);
+        }
+    }
+
+    if partition_key.is_none() {
+        panic!("{} is not found", PARTITION_KEY_FIELD);
+    }
+
+    if row_key.is_none() {
+        panic!("{} is not found", ROW_KEY_FIELD);
+    }
+
+    if timestamp.is_none() {
+        panic!("{} is not found", TIMESTAMP_FIELD);
+    }
+
+    if !partition_key.unwrap().ty.is_string() {
+        panic!("{} must be String", PARTITION_KEY_FIELD);
+    }
+
+    if !row_key.unwrap().ty.is_string() {
+        panic!("{} must be String", ROW_KEY_FIELD);
+    }
+
+    if !timestamp.unwrap().ty.is_string() {
+        panic!("{} must be String", TIMESTAMP_FIELD);
+    }
 }
