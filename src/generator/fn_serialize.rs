65a8f8f8332230dee28bd0a8fd08b13764bcf2db
amigin <amigin@gmail.com>
2022-09-03 14:40:55 +0300

    Started debugging option

--- src/generator/fn_serialize.rs
+++ src/generator/fn_serialize.rs
@@ -3,10 +3,6 @@ use crate::reflection::StructProperty;
 pub fn generate(result: &mut String, fields: &[StructProperty]) {
     result.push_str("let mut result = my_json::json_writer::JsonObjectWriter::new();");
     for field in fields {
-        if field.name == "timestamp" {
-            continue;
-        }
-
         match &field.ty {
             crate::reflection::PropertyType::U8 => {}
             crate::reflection::PropertyType::I8 => todo!(),
@@ -30,7 +26,19 @@ pub fn generate(result: &mut String, fields: &[StructProperty]) {
             crate::reflection::PropertyType::Str => todo!(),
             crate::reflection::PropertyType::Bool => todo!(),
             crate::reflection::PropertyType::DateTime => todo!(),
-            crate::reflection::PropertyType::OptionOf(_) => todo!(),
+            crate::reflection::PropertyType::OptionOf(_) => {
+                result.push_str("if let Some(value) = &self.");
+                result.push_str(&field.name);
+                result.push('{');
+
+                result.push_str("result.write_string_value(\"");
+                result.push_str(&super::db_table_name_generator(&field.name));
+                result.push_str("\", self.");
+                result.push_str(&field.name);
+                result.push_str(".as_str());\n");
+
+                result.push('}');
+            }
             crate::reflection::PropertyType::VecOf(_) => todo!(),
             crate::reflection::PropertyType::Struct(_) => todo!(),
         }
